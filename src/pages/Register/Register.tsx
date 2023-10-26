de00387745b11d499bddc46bd3121110928170cb
Long Truong <long.truong@sapotacrop.vn>
2023-10-26 11:56:38 +0700

    ui login register

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,73 @@
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+
 export default function Register() {
-    return <div>Register</div>
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data)
+    })
+    return (
+        <div className='bg-orange'>
+            <div className='max-w-7xl mx-auto px-4'>
+                <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+                    <div className='lg:col-span-2 lg:col-start-4'>
+                        <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit}>
+                            <div className='text-2xl'>Đăng Ký</div>
+                            <div className='mt-8'>
+                                <input
+                                    type='email'
+                                    name='email'
+                                    className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                                    placeholder='Email'
+                                />
+                                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                            </div>
+                            <div className='mt-5'>
+                                <input
+                                    type='password'
+                                    name='password'
+                                    className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                                    placeholder='Password'
+                                />
+                                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                            </div>
+
+                            <div className='mt-5'>
+                                <input
+                                    type='password'
+                                    name='Confilm_password'
+                                    className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                                    placeholder='Confilm Password'
+                                />
+                                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                            </div>
+
+                            <div className='mt-7'>
+                                <button
+                                    type='submit'
+                                    className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white hover:bg-red-600'
+                                >
+                                    Đăng Ký
+                                </button>
+                            </div>
+
+                            <div className='mt-4 text-center'>
+                                <div className='flex justify-center items-center mt-8'>
+                                    <span className='text-gray  -400'>Bạn có tài khoản chưa ? </span>
+                                    <Link to='/login' className='text-red-400 ml-2'>
+                                        Đăng Nhập
+                                    </Link>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
