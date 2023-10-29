93486b5209a8bc6b0885ab0a4eb1f3308a295fe6
truonghoanglong <truonghoanglongabc@gmail.com>
2023-10-29 23:22:56 +0700

    register

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
     })
     return (
         <div className='bg-orange'>
-            <div className='max-w-7xl mx-auto px-4'>
+            <div className='container'>
                 <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
                     <div className='lg:col-span-2 lg:col-start-4'>
                         <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit}>
@@ -31,6 +31,7 @@ export default function Login() {
                                 <input
                                     type='password'
                                     name='password'
+                                    autoComplete='on'
                                     className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                                     placeholder='Password'
                                 />
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,56 +1,90 @@
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import Input from '~/components/Input'
+import { getRules } from '~/utils/rules'
+
+interface FormData {
+    email: string
+    password: string
+    confirm_password: string
+}
 
 export default function Register() {
     const {
         register,
         handleSubmit,
+        getValues,
         formState: { errors }
-    } = useForm()
+    } = useForm<FormData>()
 
-    const onSubmit = handleSubmit((data) => {
-        console.log(data)
-    })
+    const onSubmit = handleSubmit(
+        (data) => {
+            // console.log(data)
+        },
+        (data) => {
+            const password = getValues('password')
+            console.log(password)
+        }
+    )
 
-    console.log('error', errors)
+    // console.log('error', errors)
+
+    const rules = getRules(getValues)
 
     return (
         <div className='bg-orange'>
-            <div className='max-w-7xl mx-auto px-4'>
+            <div className='container'>
                 <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
                     <div className='lg:col-span-2 lg:col-start-4'>
                         <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
                             <div className='text-2xl'>Đăng Ký</div>
-                            <div className='mt-8'>
-                                <input
-                                    type='email'
-                                    className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                                    placeholder='Email'
-                                    {...register('email', {
-                                        required: true
-                                    })}
-                                />
-                                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-                            </div>
-                            <div className='mt-5'>
-                                <input
-                                    type='password'
-                                    className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                                    placeholder='Password'
-                                    {...register('password')}
-                                />
-                                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-                            </div>
 
-                            <div className='mt-5'>
+                            <Input
+                                type='email'
+                                errorMessage={errors.email?.message}
+                                register={register}
+                                className='mt-8'
+                                name='email'
+                                placeholder='Email'
+                                rules={rules.email}
+                            />
+
+                            <Input
+                                type='password'
+                                errorMessage={errors.password?.message}
+                                register={register}
+                                className='mt-5'
+                                name='password'
+                                placeholder='Password'
+                                rules={rules.password}
+                                autoComplete='on'
+                            />
+
+                            <Input
+                                type='password'
+                                errorMessage={errors.confirm_password?.message}
+                                register={register}
+                                className='mt-5'
+                                name='confirm_password'
+                                placeholder='Confirm Password'
+                                rules={rules.confirm_password}
+                                autoComplete='on'
+                            />
+
+                            {/* <div className='mt-5'>
                                 <input
                                     type='password'
                                     className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                                     placeholder='Confirm Password'
-                                    {...register('confirm_password')}
+                                    autoComplete='on'
+                                    {...register('confirm_password', {
+                                        ...rules.confirm_password
+                                    })}
                                 />
-                                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-                            </div>
+                                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>
+                                    {errors.confirm_password?.message}
+                                </div>
+                            </div> */}
 
                             <div className='mt-7'>
                                 <button
